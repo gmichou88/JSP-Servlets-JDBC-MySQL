ed5af8d1ef61ff3a8b63aa120439fc3800a11e3b
gmichou88 <giwrgosmichou@gmail.com>
2021-03-22 00:52:31 +0200

    Create db_create.sql

---
+++ db_create.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE  IF NOT EXISTS `web_student_tracker`;
+USE `web_student_tracker`;
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
